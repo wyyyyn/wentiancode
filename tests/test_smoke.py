fc0e1ed6650cc0e827213d6c449fc9db3709f983
wyyyyn <yuningwanguk@gmail.com>
2026-06-11 09:30:39 +0800

    [T15/scaffold] v0.2 scaffolding: prompt_toolkit dep, version 0.2.0, ui package

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -2,4 +2,16 @@ import wentian
 
 
 def test_import():
-    assert wentian.__version__ == "0.1.0"
+    assert wentian.__version__ == "0.2.0"
+
+
+def test_version():
+    assert wentian.__version__ == "0.2.0"
+
+
+def test_ui_package():
+    import wentian.ui  # noqa: F401
+
+
+def test_prompt_toolkit_available():
+    import prompt_toolkit  # noqa: F401
